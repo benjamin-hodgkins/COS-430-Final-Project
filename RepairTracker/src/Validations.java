317be65c3b7396d022e07d9f31ccd66debe3d4c9
JPolchies <99366212+JPolchies@users.noreply.github.com>
2022-03-05 20:39:52 -0500

    Update Validations.java

--- RepairTracker/src/Validations.java
+++ RepairTracker/src/Validations.java
@@ -13,18 +13,32 @@ public class Validations {
 
 	static Pattern p = Pattern.compile("^[A-Za-z0-9-]*$");
 
-	
+
+		// Making sure the values are alpha-Numeric Only. NO special characters
+		if (p.matcher(eng).find() == false || p.matcher(clock).find() == false
+				|| p.matcher(mat).find() == false) {
+			JOptionPane
+					.showMessageDialog(null,
+							"Please use only alpha-Numeric Values only A(a)-Z(z) 0-9 or -.");
+			return false;
+		}
+		return true;
+	}
+
 	static public int userLoggingCheck(String uName, String uPassword) {
 
-		if (uName.equals("") || uPassword.equals("")) {
-
-			return 2;
-		}
-		if (uName.contains("z") || uName.contains("Z")) {
+		if (uName.equals("") && uPassword.equals("")) {
 
 			return 1;
 		}
-		return 9;
+		if(uName.equals("") || !uPassword.equals("")) {
+			return 2;
+		}
+		if(!uName.equals("") || uPassword.equals("")) {
+			return 3;
+		}
+
+		return 0;
 	}
 
 }
